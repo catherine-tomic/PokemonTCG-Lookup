f138635fdc28809f62683cdeab04c0fdb20cc4cd
catherine-tomic <cattomic@gmail.com>
2025-06-09 19:33:51 -0400

    Setup

---
+++ client/pokemon-tcg-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Expansion from './components/Expansion';
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Expansion />
+    </>
+  )
+}
+
+export default App
---
+++ client/pokemon-tcg-app/src/components/Expansion.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+
+function Expansion() {
+    return (
+       <div className="expansion-square">
+       </div> 
+    )
+}
+
+export default Expansion;
